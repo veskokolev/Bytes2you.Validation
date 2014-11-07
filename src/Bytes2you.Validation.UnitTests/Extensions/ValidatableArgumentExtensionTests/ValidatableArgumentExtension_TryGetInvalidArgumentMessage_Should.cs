85bdfdfdd06c60dcf1173c97991e8b6c4efb28cf
Vesko Kolev <vesko.kolev@gmail.com>
2014-11-07 15:28:59 +0200

    Split the Throw() method in two parts - message building and exception throwing. This enables scenarios for creating application and context specific Throw methods. For exmaple sometimes you may need to throw configuration exception -> this could be achieved easily trough creating ThrowConfigurationException() extension method.

---
+++ src/Bytes2you.Validation.UnitTests/Extensions/ValidatableArgumentExtensionTests/ValidatableArgumentExtension_TryGetInvalidArgumentMessage_Should.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using Bytes2you.Validation.Extensions;
+using Bytes2you.Validation.UnitTests.Testing.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bytes2you.Validation.UnitTests.FluentExtensions.ValidatableArgumentExtensionTests
+{
+    [TestClass]
+    public class ValidatableArgumentExcensions_TryGetInvalidArgumentMessage_Should
+    {
+        [TestMethod]
+        public void ThrowException_WhenValidatableArgumentIsNull()
+        {
+            // Arrange.
+            IValidatableArgument<int> validatableArgument = null;
+
+            // Act & Assert.
+            Ensure.ArgumentExceptionIsThrown(
+                () =>
+                {
+                    string invalidArgumentMessage;
+                    validatableArgument.TryGetInvalidArgumentMessage(out invalidArgumentMessage);
+                }, "@validatableArgument");
+        }
+
+        [TestMethod]
+        public void ReturnsFalse_WhenValidatableArgumentIsValid()
+        {
+            // Arrange.
+            IValidatableArgument<int> validatableArgument = new ValidatableArgument<int>("validatableArgument", 3);
+            validatableArgument.IsLessThan(2).IsGreaterThan(5);
+
+            // Act.
+            string invalidArgumentMessage;
+            bool result = validatableArgument.TryGetInvalidArgumentMessage(out invalidArgumentMessage);
+
+            // Assert.
+            Assert.IsFalse(result);
+            Assert.AreEqual(string.Empty, invalidArgumentMessage);
+        }
+
+        [TestMethod]
+        public void ReturnsTrueAndProperMessage_WhenValidatableArgumentIsInvalid()
+        {
+            // Arrange.
+            IValidatableArgument<int> validatableArgument = new ValidatableArgument<int>("validatableArgument", 3);
+            validatableArgument.IsGreaterThan(2).IsLessThan(5);
+
+            // Act.
+            string invalidArgumentMessage;
+            bool result = validatableArgument.TryGetInvalidArgumentMessage(out invalidArgumentMessage);
+
+            // Assert.
+            Assert.IsTrue(result);
+            Assert.AreEqual("Invalid argument:\n - Argument value 3 is greater than 2.\r\n - Argument value 3 is less than 5.\r\n", invalidArgumentMessage);
+        }
+    }
+}
---
+++ src/Bytes2you.Validation/Extensions/ValidatableArgumentExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bytes2you.Validation.Extensions
+{
+    public static class ValidatableArgumentExtensions
+    {
+        public static bool TryGetInvalidArgumentMessage<T>(this IValidatableArgument<T> @validatableArgument, out string invalidArgumentMessage)
+        {
+            if (@validatableArgument == null)
+            {
+                throw new ArgumentNullException("@validatableArgument");
+            }
+
+            List<IValidationResult> invalidValidationResults = new List<IValidationResult>();
+            foreach (IValidationRule<T> validationRule in @validatableArgument.ValidationRules)
+            {
+                IValidationResult validationResult = validationRule.Validate(@validatableArgument.Value);
+                if (validationResult.IsValid)
+                {
+                    invalidValidationResults.Add(validationResult);
+                }
+            }
+
+            if (invalidValidationResults.Count > 0)
+            {
+                StringBuilder messageBuilder = new StringBuilder("Invalid argument:\n");
+
+                foreach (IValidationResult validationResult in invalidValidationResults)
+                {
+                    messageBuilder.AppendLine(string.Format(" - {0}", validationResult.Message));
+                }
+
+                invalidArgumentMessage = messageBuilder.ToString();
+            }
+            else
+            {
+                invalidArgumentMessage = string.Empty;
+            }
+
+            return !string.IsNullOrEmpty(invalidArgumentMessage);
+        }
+    }
+}
