a10939064a37da058f04a7aa49bc623ad8da56aa
Vesko Kolev <vesko.kolev@gmail.com>
2014-11-11 19:06:25 +0200

    Updated the assembly version.

--- src/Bytes2you.Validation/Properties/AssemblyInfo.cs
+++ src/Bytes2you.Validation/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("47264e79-a279-4665-8e28-ef617ccb1b8f")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 #if RELEASEOFFICIAL
 [assembly: InternalsVisibleTo("Bytes2you.Validation.UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100fb4c5f265a35a3663c6df836e7ad646764d02962973f8d7b3584f35f1d68b8f0beb4f5a3a21140a3051bdd4b83e66ab86c1379ec237b89286f79a591ca975f73a06a60d4b5c232131add54ecc7e3e568da05b7fefde172961baa5d10736fad3b88f3d4b105cae46234ebb9b442b902a535fd78405ef972c17199549b735f72ea")]
