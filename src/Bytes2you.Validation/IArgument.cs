50281b1a5b9485a2b839d5a92929b51c4bb4a0a4
Vesko Kolev <vesko.kolev@gmail.com>
2014-11-21 16:41:59 +0200

    IntelliSense improvements.

--- src/Bytes2you.Validation/IArgument.cs
+++ src/Bytes2you.Validation/IArgument.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Bytes2you.Validation
 {
     public interface IArgument<T>
     {
+        [EditorBrowsable(EditorBrowsableState.Never)]
         string Name { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
         T Value { get; }
     }
 }
--- src/Bytes2you.Validation/IValidatableArgument.cs
+++ src/Bytes2you.Validation/IValidatableArgument.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Bytes2you.Validation
 {
     public interface IValidatableArgument<T> : IArgument<T>
     {
+        [EditorBrowsable(EditorBrowsableState.Never)]
         IEnumerable<IValidationPredicate<T>> ValidationPredicates { get; }
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
         void AddValidationPredicate(IValidationPredicate<T> validationPredicate);
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
         IEnumerable<IValidationPredicateResult> MatchValidationPredicates();
     }
 }
